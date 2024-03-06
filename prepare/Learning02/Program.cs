bc12cc9f9b32a1dff239c7b43b4253947ca89dd8
curtissiahe18@gmail.com <curtissiahe18@gmail.com>
2024-03-06 10:20:57 +0000

    made

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1=new Job();
+       job1._jobTitle="Software Engineer";
+       job1._companyName="Microsoft";
+       job1._startYear=2005;
+       job1._startYear=2020;
+
+
+       Job job2= new Job();
+       job2._jobTitle="Software Engineer";
+       job2._companyName="Apple";
+       job2._startYear=2007;
+       job2._startYear=2022;
     }
 }
